e376a6c91966fe0028539bc18647d496f82e211d
marshall-dieme <marshalldieme@gmail.com>
2022-12-16 23:50:37 +0000

    Ajout de l'Image pour futur upload

---
+++ generer-mensualite/src/main/java/sn/ksb/immo/generermensualite/GenererMensualiteApplication.java
@@ -0,0 +1,13 @@
+package sn.ksb.immo.generermensualite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenererMensualiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenererMensualiteApplication.class, args);
+    }
+
+}
---
+++ generer-mensualite/src/test/java/sn/ksb/immo/generermensualite/GenererMensualiteApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ksb.immo.generermensualite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenererMensualiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
