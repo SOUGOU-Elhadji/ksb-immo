5d627ff95d81772a24fbef8f2412ca27bc0dc794
Marshall Dieme <58795947+marshall-dieme@users.noreply.github.com>
2022-12-28 20:39:23 +0000

    Delete generate-contract 23.02.07 directory
    Removing old directory

--- generate-contract 23.02.07/src/main/java/sn/ksb/immo/ksbimmo/generatecontract/repository/FileRepo.java
+++
@@ -1,7 +0,0 @@
-package sn.ksb.immo.ksbimmo.generatecontract.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sn.ksb.immo.ksbimmo.generatecontract.model.File;
-
-public interface FileRepo extends JpaRepository<File,String> {
-}
